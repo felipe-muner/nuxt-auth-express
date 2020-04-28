8ec114e27768d12c61c3c066e39127f3effd6d4b
felipemuner <felipe.muner@gmail.com>
2020-04-29 00:39:44 +0700

    set loading component inside axios interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,16 +2,17 @@ export default function({ $axios, redirect }) {
   $axios.setBaseURL("http://localhost:8081");
 
   $axios.onRequest(config => {
-    console.log(config);
-    console.log("Making request to " + config.url);
+    $nuxt.$loading.start();
+
+    // console.log(config);
+    // console.log("Making request to " + config.url);
   });
 
   $axios.onResponse(config => {
-    console.log(config);
-    console.log("Coming back from response to");
+    $nuxt.$loading.finish();
+    // console.log(config);
+    // console.log("Coming back from response to");
   });
 
-  $axios.onError(error => {
-    console.log(error);
-  });
+  $axios.onError(error => {});
 }
