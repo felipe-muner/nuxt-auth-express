a728511855cb5252df72096ecc7d345ab1f52e51
felipemuner <felipe.muner@gmail.com>
2020-05-05 09:42:11 +0700

    refactor controller auth to user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const AuthenticationController = require("../controllers/AuthenticationController");
+const UserController = require("../controllers/UserController");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.send({ msg: "ok" });
 });
 
-router.post("/login", AuthenticationController.login);
+router.post("/login", UserController.login);
 
-router.post("/register", AuthenticationController.register);
+router.post("/register", UserController.register);
 
-router.get("/user", AuthenticationController.verifyUser);
+router.get("/user", UserController.verifyUser);
 
-router.get("/logout", AuthenticationController.logout);
+router.get("/logout", UserController.logout);
 
 module.exports = router;
